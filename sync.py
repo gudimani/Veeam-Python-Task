8e9492ac4949b97494c0013f06c14eba6f92696b
gudimani <60035862+gudimani@users.noreply.github.com>
2023-05-06 01:23:12 +0200

    sync source to replica recursively

--- sync.py
+++ sync.py
@@ -1,8 +1,78 @@
 import argparse
+import os
+import time
+import shutil
+import datetime
+import hashlib
 
 
-def sync(source, replica, interval, log_file):
-    print(source, replica, interval, log_file)
+def sync(src_path, dest):
+    # TODO catch path does not exist error
+    # check source is proper or not ---yet to do
+    src_path_2 = src_path
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+        print(f"Directory '{dest}' created")
+        # check for files or folders in source
+        shutil.copytree(src_path, dest)
+        # for file in os.listdir(source):
+        #     source_file_path = os.path.join(source, file)
+        #     replica_file_path = os.path.join(replica, file)
+        #     # shutil.copy2(os.path.join(source, file), replica) is throwing permission error while copying folders
+        #     # TODO-- copy entire directory
+        #     if os.path.isdir(source_file_path):
+        #         shutil.copytree(source_file_path, replica_file_path)
+
+        #     elif os.path.isfile(source_file_path):
+        #         shutil.copy2(source_file_path, replica_file_path)
+
+        #     else:
+        #         print(
+        #             "copy other than files/folders to replica, edge cases such as .git etc")
+
+    else:
+        print(f"Directory '{dest}' already exists")
+        src_path_len = len(src_path)
+        for root, dirs, files in os.walk(src_path):
+            # print(root, dirs, files)
+
+            for file in files:
+                src_path = os.path.join(root, file)
+
+                dest_append = root[src_path_len:]
+
+                dest_path = os.path.join(dest+dest_append, file)
+                if not os.path.exists(dest_path):
+                    shutil.copy2(src_path, dest_path)
+                else:
+                    with open(src_path, 'rb') as s:
+                        contents = s.read()
+                        hash_object = hashlib.md5(contents)
+                        md5_hash_source = hash_object.hexdigest()
+                    with open(dest_path, 'rb') as d:
+                        contents = d.read()
+                        hash_object = hashlib.md5(contents)
+                        md5_hash_dest = hash_object.hexdigest()
+                    if md5_hash_source != md5_hash_dest:
+                        shutil.copy2(src_path, dest_path)
+            for dir in dirs:
+                dest_append = root[src_path_len:]
+                dest_dir_path = os.path.join(dest+dest_append, dir)
+                if not os.path.exists(dest_dir_path):
+                    os.makedirs(dest_dir_path)
+
+        # deletefiles at replica but not at source
+        for file in os.listdir(dest):
+            if not os.path.exists(os.path.join(src_path_2, file)):
+                replica_file_path = os.path.join(dest, file)
+                print("replica_file_path", replica_file_path)
+                if os.path.isdir(replica_file_path):
+                    shutil.rmtree(replica_file_path)
+                    print("folder removed from replica")
+                else:
+                    os.remove(replica_file_path)
+                    print("file removed from replica")
+        # Compare files in both add if not present at replica, if present check for the modifications at source and update at replica
 
 
 if __name__ == "__main__":
@@ -17,8 +87,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     source = args.source
-    replica = args.replica
+    dest = args.replica
     interval = args.interval
     log_file = args.log_file
 
-    sync(source, replica, interval, log_file)
+    while True:
+
+        sync(source, dest)
+        time.sleep(60)
