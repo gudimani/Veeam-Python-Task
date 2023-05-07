d63fa9ff285c35d01c51bcc3b414ba97022d725d
gudimani <60035862+gudimani@users.noreply.github.com>
2023-05-07 23:42:50 +0200

    update naming convention

--- test_cases.py
+++ test_cases.py
@@ -1,14 +1,30 @@
-import pytest
-import sync
-import os
 import hashlib
+import os
+import sync
 
 
-def test_source_path():
+# wrong source input should result in console output message
+def test_source_path(capfd, tmpdir):
 
-    with pytest.raises(FileNotFoundError):
-        sync.main('\\wrong\\path',
-                  'C:\\Users\\Pavitra\\Desktop\\Veeam\\Veeam-Python-Task\\replica', '1', 'log_new.txt')
+    dest = tmpdir.join("replica")
+    dest.mkdir()
+    sync.main('\\wrong\\path',
+              str(dest), 1, 'log_new.txt')
+    out, err = capfd.readouterr()
+    assert out == 'Source path does not exist. Please enter valid source path.\n'
+
+# wrong source input should result in console output message
+
+
+def test_interval(capfd, tmpdir):
+    source = tmpdir.join("source")
+    dest = tmpdir.join("replica")
+    source.mkdir()
+    dest.mkdir()
+    sync.main(str(source), str(dest), -1, 'log_new.txt')
+    out, err = capfd.readouterr()
+    assert out == 'Please provide positive interval value\n'
+
 
 # dest does not exist, then creating and copying
 
@@ -39,7 +55,7 @@ def test_file_exists(tmpdir):
     dest_file = dest / "pytest.txt"
     assert os.path.exists(dest_file)
 
-# dest exists - dest has file but not at source
+# dest exists - dest has file but not at source, so file is removed from dest
 
 
 def test_file_removal(tmpdir):
@@ -53,13 +69,13 @@ def test_file_removal(tmpdir):
     sync.sync(str(source), str(dest))
     assert not os.path.exists(dest_file)
 
-# source file modified and update at replica
+# File is present at both source and dest
+# but when source file is modified,the file at replica is updated
 
 
 def test_file_modification(tmpdir):
     CONTENT_source = "source content"
     CONTENT_dest = "dest content"
-
     source = tmpdir.join("source")
     dest = tmpdir.join("replica")
     source.mkdir()
@@ -79,21 +95,23 @@ def test_file_modification(tmpdir):
         md5_hash_dest = hash_object.hexdigest()
     assert md5_hash_source == md5_hash_dest
 
-# test folder existing at source but not at replica
+# a subfolder existing at source but not at replica,
+# the subfolder is created at replica
 
 
 def test_folder(tmpdir):
     source = tmpdir.join("source")
     source.mkdir()
-    extra = source.join("extra")
+    subfolder = source.join("subfolder")
     dest = tmpdir.join("replica")
-    extra.mkdir()
+    subfolder.mkdir()
     dest.mkdir()
     sync.sync(str(source), str(dest))
-    dest_dir = dest.join("extra")
+    dest_dir = dest.join("subfolder")
     assert os.path.exists(dest_dir)
 
-# test subdirectories existing at replica but not at source
+# test subfolder existing at replica but not at source,
+# subfolder is removed from replica
 
 
 def test_removal_folder(tmpdir):
@@ -101,49 +119,50 @@ def test_removal_folder(tmpdir):
     source.mkdir()
     dest = tmpdir.join("replica")
     dest.mkdir()
-    extra = dest.join("extra")
-    extra.mkdir()
+    subfolder = dest.join("subfolder")
+    subfolder.mkdir()
     sync.sync(str(source), str(dest))
-    assert not os.path.exists(extra)
+    assert not os.path.exists(subfolder)
 
-# copy files at subdir of source
+# when new file created at source in a subfolder,
+# create the same file in replica
 
 
 def test_folder_file(tmpdir):
     CONTENT = "subfolder text"
     source = tmpdir.join("source")
     source.mkdir()
-    source_extra = source.join("extra")
-    source_extra.mkdir()
-    source_extra_file = source_extra / "subfolder.txt"
-    source_extra_file.write_text(CONTENT, encoding='utf-8')
-
+    source_subfolder = source.join("subfolder")
+    source_subfolder.mkdir()
+    source_subfolder_file = source_subfolder / "subfolder.txt"
+    source_subfolder_file.write_text(CONTENT, encoding='utf-8')
     dest = tmpdir.join("replica")
     dest.mkdir()
-
     sync.sync(str(source), str(dest))
-    dest_extra = dest.join("extra")
-    dest_extra_file = dest_extra / "subfolder.txt"
-    assert os.path.exists(dest_extra_file)
+    dest_subfolder = dest.join("subfolder")
+    dest_subfolder_file = dest_subfolder / "subfolder.txt"
+    assert os.path.exists(dest_subfolder_file)
 
-# remove files of subdir at replica but not at source
+# remove files present in a subfolder at replica but not at source
 
 
 def test_removal_folder_file(tmpdir):
     CONTENT = "subfolder dest content"
     source = tmpdir.join("source")
     source.mkdir()
-    source_extra = source.join("extra")
-    source_extra.mkdir()
+    source_subfolder = source.join("subfolder")
+    source_subfolder.mkdir()
 
     dest = tmpdir.join("replica")
     dest.mkdir()
-    dest_extra = dest.join("extra")
-    dest_extra.mkdir()
-    dest_extra_file = dest_extra / "subfolder.txt"
-    dest_extra_file.write_text(CONTENT, encoding='utf-8')
+    dest_subfolder = dest.join("subfolder")
+    dest_subfolder.mkdir()
+    dest_subfolder_file = dest_subfolder / "subfolder.txt"
+    dest_subfolder_file.write_text(CONTENT, encoding='utf-8')
     sync.sync(str(source), str(dest))
-    assert not os.path.exists(dest_extra_file)
+    assert not os.path.exists(dest_subfolder_file)
+
+# a file present in a subfolder at source is modified should be updated in replica
 
 
 def test_folder_file_modification(tmpdir):
@@ -151,23 +170,22 @@ def test_folder_file_modification(tmpdir):
     CONTENT_dest = "Subfolder dest content"
     source = tmpdir.join("source")
     source.mkdir()
-    source_extra = source.join("extra")
-    source_extra.mkdir()
-    source_extra_file = source_extra / "subfolder.txt"
-    source_extra_file.write_text(CONTENT_source, encoding='utf-8')
-
+    source_subfolder = source.join("subfolder")
+    source_subfolder.mkdir()
+    source_subfolder_file = source_subfolder / "subfolder.txt"
+    source_subfolder_file.write_text(CONTENT_source, encoding='utf-8')
     dest = tmpdir.join("replica")
     dest.mkdir()
-    dest_extra = dest.join("extra")
-    dest_extra.mkdir()
-    dest_extra_file = dest_extra / "subfolder.txt"
-    dest_extra_file.write_text(CONTENT_dest, encoding='utf-8')
+    dest_subfolder = dest.join("subfolder")
+    dest_subfolder.mkdir()
+    dest_subfolder_file = dest_subfolder / "subfolder.txt"
+    dest_subfolder_file.write_text(CONTENT_dest, encoding='utf-8')
     sync.sync(str(source), str(dest))
-    with open(source_extra_file, 'rb') as s:
+    with open(source_subfolder_file, 'rb') as s:
         content = s.read()
         hash_object = hashlib.md5(content)
         md5_hash_source = hash_object.hexdigest()
-    with open(dest_extra_file, 'rb') as d:
+    with open(dest_subfolder_file, 'rb') as d:
         content = d.read()
         hash_object = hashlib.md5(content)
         md5_hash_dest = hash_object.hexdigest()
